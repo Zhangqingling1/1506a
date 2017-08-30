00cf94669e0274bd78bdc0b74ecb2063e3a5ca3a
ZhangqinglingA <17600920523@189.cn>
2017-08-30 10:35:30 +0800

    Initial commit

---
+++ app/src/main/java/com/aqinga/gitapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aqinga.gitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //haha
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
